62da795ef982338bf621fa382771bf6ed6c8cd1a
ray <ray@raylios.com>
2018-12-13 20:28:00 +0800

    Exchange image url with token

--- retail-ai-web/scripts/main.js
+++ retail-ai-web/scripts/main.js
@@ -237,7 +237,7 @@ function getImgUrl(token, urlPath){
 
     $.ajax(settings).done(function (response) {
         console.log(JSON.stringify(response));
-        urlPath=response.Location;
+        headshotPath=response.Location;
         updateImage();
     });    
 }
