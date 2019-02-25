25e4d076d1558a54dfed0208e5d599a6bd4cfa03
sonia <sonia.lin@kantech.com.tw>
2019-02-26 00:04:30 +0800

    test member.js

--- retail-ai-web/dist/scripts/member.js
+++ retail-ai-web/dist/scripts/member.js
@@ -1,4 +1,89 @@
 
 var memberImgsInfo = JSON.parse(localStorage.getItem("memberImgs"));
-// console.log(memberImgsInfo);
+console.log(memberImgsInfo);
 
+for (var i = 0; i < memberImgsInfo.length; i++) {
+    const element = memberImgsInfo[i];
+    var cvsId="canvas"+i;
+    var html = "<div class='snapshot_wrap'>" +
+                    "<canvas id='" + cvsId + "'></canvas>" +
+                    "<a href='#' class='btn_snapshotDel'>X</a>" +
+                "</div>";
+                
+    $(".memberInfo_snapshots").append(html);
+}
+
+uploadJpeg();
+
+function uploadJpeg(uploadUrl) {
+
+    console.log("===== uploadJpeg ==========")
+    console.log("upload to url:" + uploadUrl)
+
+    for (var i = 0; i < memberImgsInfo.length; i++) {
+        var id = "canvas" + i;
+        var left = memberImgsInfo[i].faceRectangles[0].left;
+        var top = memberImgsInfo[i].faceRectangles[0].top;
+        var w = memberImgsInfo[i].faceRectangles[0].width;
+        var h = memberImgsInfo[i].faceRectangles[0].height;
+        var src = "https://di93lo4zawi3i.cloudfront.net/" + memberImgsInfo[i].imgToken;
+        drawCanvas(id, src, w, h, top, left);
+        console.log(i)
+        wait()
+    }
+
+}
+
+function drawCanvas(id, src, w, h, top, left) {
+    var image = new Image();
+
+    var canvas = document.getElementById(id);
+    ctx = canvas.getContext('2d');
+
+    image.src = src;
+    image.setAttribute("crossOrigin", 'Anonymous');
+
+    image.onload = function () {
+        console.log(left);
+        let imageRect = {
+            left: left,
+            top: top,
+            width: w,
+            height: h
+        };
+        let canvasRect = {
+            left: 0,
+            top: 0,
+            width: canvas.width,
+            height: canvas.height
+        };
+
+        cropImgToCanvas(image, canvas, imageRect, canvasRect);
+        // ctx.drawImage(image,
+        //     70, 20,   // Start at 70/20 pixels from the left and the top of the image (crop),
+        //     50, 50,   // "Get" a `50 * 50` (w * h) area from the source image (crop),
+        //     0, 0,     // Place the result at 0, 0 in the canvas,
+        //     50, 50); // With as width / height: 100 * 100 (scale)
+    }
+
+
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function wait() {
+    console.log('Taking a break...');
+    await sleep(2000);
+    console.log('Two seconds later');
+}
+
+function cropImgToCanvas(img, canvas, rectangle, drawRectangle) {
+    ctx = canvas.getContext('2d');
+    ctx.drawImage(img,
+        rectangle.left, rectangle.top,   // Start at 70/20 pixels from the left and the top of the image (crop),
+        rectangle.width, rectangle.height,   // "Get" a `50 * 50` (w * h) area from the source image (crop),
+        drawRectangle.left, drawRectangle.top,     // Place the result at 0, 0 in the canvas,
+        drawRectangle.width, drawRectangle.height); // With as width / height: 100 * 100 (scale)
+}
