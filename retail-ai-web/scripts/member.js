a2ff99c7682413ec7a4ccb690b0082ca2980137e
zongenyu <zongenyu@gmail.com>
2019-03-06 12:23:17 +0800

    bug fix for taken htmlCollections as array

--- retail-ai-web/scripts/member.js
+++ retail-ai-web/scripts/member.js
@@ -244,6 +244,7 @@ var saveJpegNote = function(){
 
     console.log("===== Start upload All Jpeg ==========")
     var divs = document.getElementsByClassName('snapshot_wrap')
+    faces=[]
 
     for (let i=0;i<divs.length; i++) {
 
